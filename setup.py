59260ac212b9ebe637f80ccf43cb6f085e816cd4
gQuantCoder <gquantcoder@gmail.com>
2019-09-17 17:00:37 -0400

    better setup

--- setup.py
+++ setup.py
@@ -1,33 +1,33 @@
-import pathlib
-import setuptools
+import runpy
+from setuptools import setup, find_packages
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-# The text of the README file
-README = (HERE / "README.md").read_text()
+PACKAGE_NAME = "deep_orderbook"
+version_meta = runpy.run_path("./version.py")
+VERSION = version_meta["__version__"]
 
-requirements = ["tqdm", "python_binance"]
 
-packages = setuptools.find_packages()
-print("packages:", packages)
-setuptools.setup(
-    name="deep_orderbook",
-    version="0.0.1",
-    description="Transforms orders books in temporally and spatially local-correlated images",
-    long_description=README,
-    long_description_content_type="text/markdown",
-    url="https://github.com/gQuantCoder/deep_orderbook",
-    author="gQuantCoder",
-    author_email="gquantcoder@gmail.com",
-    license="MIT",
-    classifiers=[
-        # "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
-    packages=packages,
-    include_package_data=True,
-    install_requires=requirements,
-    entry_points={"console_scripts": ["deepbook=deep_orderbook.__main__:main"]},
-)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file."""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+if __name__ == "__main__":
+    setup(
+        name=PACKAGE_NAME,
+        version=VERSION,
+        description="This is a description.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/gQuantCoder/deep_orderbook",
+        author="gQuantCoder",
+        author_email="gquantcoder@gmail.com",
+        packages=find_packages(),
+        install_requires=parse_requirements("requirements.txt"),
+        python_requires=">=3.6.3",
+        entry_points={"console_scripts": ["deepbook=deep_orderbook.__main__:main"]},
+    )
