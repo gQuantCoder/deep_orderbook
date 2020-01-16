71d87a9f19a4c5d2cd35468ff8bcf7d28a89b638
gQuantCoder <gquantcoder@gmail.com>
2020-01-15 20:57:17 -0500

    works but msg is received in the DepthManager instance, not directly accessible for saving

--- deep_orderbook/recorder.py
+++ deep_orderbook/recorder.py
@@ -8,7 +8,7 @@ import copy
 import threading
 import tqdm
 import asyncio
-from binance.client import Client  # Import the Binance Client
+from binance import AsyncClient, DepthCacheManager # Import the Binance Client
 
 # Import the Binance Socket Manager
 from binance.websockets import BinanceSocketManager
@@ -20,44 +20,66 @@ from binance.exceptions import BinanceAPIException
 class Receiver:
     def __init__(self, markets):
         self.markets = markets
+        asyncio.get_event_loop().run_until_complete(self.setup())
 
+    async def setup(self):
         # Instantiate a Client
-        self.client = Client(api_key='PUBLIC', api_secret='SECRET')
+        self.client = await AsyncClient.create()
+        print(json.dumps(await self.client.get_exchange_info(), indent=2))
+        for m in self.markets:
+            print(json.dumps(await self.client.get_symbol_ticker(symbol=m), indent=2))
 
         # Instantiate a BinanceSocketManager, passing in the client that you instantiated
-        self.bm = BinanceSocketManager(self.client)
+        self.bm = BinanceSocketManager(self.client, loop=asyncio.get_event_loop())
         self.nummsg = collections.defaultdict(int)
         self.conn_keys = []
+        self.depth_managers = {}
 
-    def on_depth(self, msg):
-        symbol = msg["s"]
-#        print('no bid' if not msg['b'] else '', 'no ask' if not msg['a'] else '')
+    async def on_depth(self, depth_cache):
+#        print(f"symbol {depth_cache.symbol} updated:{depth_cache.update_time}")
+#        print("Top 5 asks:", len(depth_cache.get_asks()))
+        print(depth_cache.get_asks()[:5])
+#        print("Top 5 bids:", len(depth_cache.get_bids()))
+        print(depth_cache.get_bids()[:5])
+        symbol = depth_cache.symbol
+        # print('no bid' if not depth_cache.get_bids() else '', 'no ask' if not depth_cache.get_asks() else '')
         self.nummsg[symbol] += 1
         print(', '.join([f'{s}: {self.nummsg[s]:06}' for s in self.markets]), end='\r')
 
 
-    def on_aggtrades(self, msg):
+    async def on_aggtrades(self, msg):
+        #print(msg)
         symbol = msg["s"]
         self.nummsg[symbol] += 1
 
-    def stoprestart(self, dorestart=True):
+    async def stoprestart(self, dorestart=True):
         # stop the socket manager
         for conn_key in self.conn_keys:
             print(f"stopping socket {conn_key}\n")
-            self.bm.stop_socket(conn_key)
+            await self.bm.stop_socket(conn_key)
+        self.depth_managers = {}
         conn_keys = []
-        self.bm.close()
+#        await self.bm.close()
 
         if dorestart:
             for symbol in self.markets:
                 #os.makedirs(f"{DATA_FOLDER}/L2/{symbol}", exist_ok=True)
-                key = self.bm.start_depth_socket(symbol, self.on_depth)  # , depth='20')
+#                key = await self.bm.start_depth_socket(symbol, self.on_depth)  # , depth='20')
+#                print("start", key)
+#                conn_keys.append(key)
+                key = await self.bm.start_aggtrade_socket(symbol, self.on_aggtrades)
                 print("start", key)
                 conn_keys.append(key)
-                key = self.bm.start_aggtrade_socket(symbol, self.on_aggtrades)
-                print("start", key)
-                conn_keys.append(key)
-            self.bm.start()
+                # create the Depth Cache
+                depthmanager = await DepthCacheManager.create(self.client, asyncio.get_event_loop(), 
+                                                                                symbol, 
+                                                                                self.on_depth,
+                                                                                bm=self.bm,
+                                                                                limit=5000
+                                                                                )
+                self.depth_managers[symbol] = depthmanager
+
+            #await self.bm.start()
 
 class Writer(Receiver):
     def __init__(self, markets, data_folder):
@@ -72,11 +94,11 @@ class Writer(Receiver):
         for symbol in self.markets:
             os.makedirs(f"{self.L2folder}/{symbol}", exist_ok=True)
 
-    def on_depth(self, msg):
-        super().on_depth(msg)
-        symbol = msg["s"]
+    def on_depth(self, depth_cache):
+        super().on_depth(depth_cache)
+        symbol = depth_cache.symbol
         with self.lock:
-            self.store[symbol].append(msg)
+            self.store[symbol].append(depth_cache.msg)
 
     def on_aggtrades(self, msg):
         super().on_aggtrades(msg)
@@ -84,7 +106,7 @@ class Writer(Receiver):
         with self.tradelock:
             self.tradestore[symbol].append(msg)
 
-    def save_snapshot(self, cur_ts, prev_ts):
+    async def save_snapshot(self, cur_ts, prev_ts):
         progressbar = self.markets
         for symbol in progressbar:
             snap = datetime.datetime.utcfromtimestamp(cur_ts).isoformat().replace(":", "-")  # .replace('-',"_")
@@ -107,7 +129,7 @@ class Writer(Receiver):
             for symbol in progressbar:
                 print(symbol)
                 # time.sleep(1)
-                L2 = self.client.get_order_book(symbol=symbol, limit=1000)
+                L2 = await self.client.get_order_book(symbol=symbol, limit=1000)
                 with open(f"{self.L2folder}/{symbol}/{snap}_snapshot.json", "w") as fp:
                     json.dump(L2, fp)
         print("\nsaved_snapshot \n")
@@ -136,17 +158,17 @@ class Writer(Receiver):
 
                 if datetime.datetime.now() >= stopat:
                     # saves for next period that will be overwritten by next script
-                    self.save_snapshot(0, prev_th)
+                    await self.save_snapshot(0, prev_th)
                     print("\nexiting\n", "s =", s, "\n")
                     break
 
                 if new_th > th:
                     print("\n", ti, datetime.datetime.fromtimestamp(ti))
                     if th == 0:
-                        self.save_snapshot(ti, ti)
+                        await self.save_snapshot(ti, ti)
                         prev_th = ti
                     else:
-                        self.save_snapshot(new_th, prev_th)
+                        await self.save_snapshot(new_th, prev_th)
                         prev_th = new_th
                     th = new_th
         except Exception as e:
@@ -158,4 +180,7 @@ class Writer(Receiver):
 
 if __name__ == '__main__':
     rec = Receiver(['ETHBTC'])
-    rec.stoprestart()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(rec.stoprestart())
+    while True:
+        loop.run_until_complete(asyncio.sleep(10))
